c7e86ec57cf4f10094c067cd390d6d2e4b142402
franknstin <anmhatre94@gmail.com>
2023-03-22 07:37:21 +0530

    added entity

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from housing.logger import logging
 
 # create an instance of the Flask class with the name of the current module
 app = Flask(__name__)
@@ -6,6 +7,7 @@ app = Flask(__name__)
 # define a route for the path '/' with methods 'GET' and 'POST'
 @app.route("/", methods=["GET", "POST"])
 def index():
+    logging.info("we are testing logging module")
     return "starting machine learning project"
 
 # run the application in debug mode if this file is executed directly
